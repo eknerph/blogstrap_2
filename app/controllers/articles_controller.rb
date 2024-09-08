b713e6f694926e5ea444c51d14ab8237deb2576e
eknerph <eknerph@gmail.com>
2024-09-08 01:03:41 -0300

    Mostra um Article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,62 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = Article.all
+  @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
